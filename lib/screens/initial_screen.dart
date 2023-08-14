5e8193790b9511631a02c77e149f8ca71b3327cb
Jhonatta Silva <jhonatta.ferreira96@outlook.com>
2023-08-14 20:35:25 -0300

    First Commit

---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:nosso_primeiro_projeto/components/task.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool opacidade = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          title: const Text('Tarefas'),
+        ),
+        body: AnimatedOpacity(
+          opacity: opacidade ? 1 : 0,
+          duration: const Duration(milliseconds: 800),
+          child: ListView(
+            children: const [
+              Task(
+                  'Aprender Flutter',
+                  'assets/images/dashdart.png',
+                  3),
+              Task(
+                  'Andar de Bike',
+                  'assets/images/bike.jpg',
+                  2),
+              Task(
+                  'Meditar',
+                  'assets/images/meditar.jpeg',
+                  4),
+              Task(
+                  'Ler',
+                  'assets/images/ler.jpg',
+                  1),
+              Task(
+                  'Jogar',
+                  'assets/images/kako.jpg',
+                  1),
+              SizedBox(
+                height: 80,
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              opacidade = !opacidade;
+            });
+          },
+          child: const Icon(Icons.remove_red_eye),
+        ));
+  }
+}
